4a0794ef80a5e4003b1b8a3e5e98ddc54c9730a4
Kongo67 <kongo@aucegypt.edu>
2023-08-15 16:17:33 +0300

    First commit containing structure of project

---
+++ src/main/java/com/vois/traininghub/TraininghubApplication.java
@@ -0,0 +1,13 @@
+package com.vois.traininghub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraininghubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraininghubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vois/traininghub/TraininghubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vois.traininghub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraininghubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
