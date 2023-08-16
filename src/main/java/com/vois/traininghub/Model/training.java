4deb48fd8338eaf5fa678949d11eb13939e3f128
jessicamansi <jessicamansi@aucegypt.edu>
2023-08-16 14:03:36 +0300

    API insert training working

--- src/main/java/com/vois/traininghub/Model/training.java
+++ src/main/java/com/vois/traininghub/Model/training.java
@@ -2,14 +2,14 @@ package com.vois.traininghub.Model;
 
 import jakarta.persistence.*;
 
-import java.util.List;
+//import java.util.List;
 
 @Entity
 @Table(name = "Trainings")
-public class training{
+public class training {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "Training_ID")
     private long id;
 
@@ -31,14 +31,15 @@ public class training{
     @Column(name = "AVG_Rating")
     public long AVG_Rating;
 
-    @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name = "FK_Training_ID", referencedColumnName = "Training_ID")
-    private List<feedback> Train_ID;
+    // @OneToMany(cascade = CascadeType.ALL)
+    // @JoinColumn(name = "FK_Training_ID", referencedColumnName = "Training_ID")
+    // private List<feedback> Train_ID;
 
     public training() {
     }
 
     public training(String name, String link, long duration, String topic, String entity, long AVG_Rating) {
+
         this.name = name;
         this.link = link;
         this.duration = duration;
@@ -55,4 +56,5 @@ public class training{
         this.id = id;
         System.out.println("ID set to: " + this.id);
     }
+
 }
---
+++ src/main/java/com/vois/traininghub/Repository/TrainingRepository.java
@@ -0,0 +1,23 @@
+package com.vois.traininghub.Repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vois.traininghub.Model.training;;
+
+public interface TrainingRepository extends JpaRepository<training, Long> {
+    // List<training> findById(long id);
+
+    List<training> findByName(String name);
+
+    // List<training> FindBylink(String link);
+
+    // List<training> findbyduration(long duration);
+
+    // List<training> findbytopic(String topic);
+
+    // List<training> findbyentity(String entity);
+
+    // List<training> findbyrate(long AVG_Rating);
+}
