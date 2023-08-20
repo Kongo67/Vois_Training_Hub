ce37aed357da21511f59636cfc7e15447d101d44
jessicamansi <jessicamansi@aucegypt.edu>
2023-08-20 14:08:01 +0300

    JE-04 updated query params for get

--- src/main/java/com/vois/traininghub/Controller/TrainingController.java
+++ src/main/java/com/vois/traininghub/Controller/TrainingController.java
@@ -1,7 +1,9 @@
 package com.vois.traininghub.Controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -28,30 +30,51 @@ public class TrainingController {
     @Autowired
     TrainingRepository trainingRepository;
 
-        @GetMapping("/training")
-        public ResponseEntity<?> getTraining(@RequestParam(value = "id", required = false, defaultValue = "0") long id) {
-            if (id == 0) {
-                try {
-                    List<training> trainings = trainingRepository.findAll();
-                
-                    if (trainings.isEmpty()) {
-                        return new ResponseEntity<>("No trainings found.", HttpStatus.NOT_FOUND);
-                    }
-                
-                    return new ResponseEntity<>(trainings, HttpStatus.OK);
-                } catch (Exception e) {
-                    return new ResponseEntity<>("An error occurred, please enter the ID again.", HttpStatus.INTERNAL_SERVER_ERROR);
-                }
-            } else {
-                Optional<training> trainingData = trainingRepository.findById(id);
-            
-                if (trainingData.isPresent()) {
-                    return new ResponseEntity<>(trainingData.get(), HttpStatus.OK);
-                } else {
-                    return new ResponseEntity<>("Training not found.", HttpStatus.NOT_FOUND);
-                }
+    @GetMapping("/training")
+    public ResponseEntity<?> getTraining(
+            @RequestParam(value = "id", required = false, defaultValue = "0") long id,
+            @RequestParam(value = "name", required = false) String name,
+            @RequestParam(value = "entity", required = false) String entity,
+            @RequestParam(value = "topic", required = false) String topic) {
+
+        List<training> filteredTrainings = new ArrayList<>();
+
+        if (id != 0) {
+            Optional<training> trainingData = trainingRepository.findById(id);
+
+            if (trainingData.isPresent()) {
+                filteredTrainings.add(trainingData.get());
             }
+        } else {
+            filteredTrainings = trainingRepository.findAll();
         }
+
+        if (name != null) {
+            filteredTrainings = filteredTrainings.stream()
+                    .filter(training -> training.getName().equals(name))
+                    .collect(Collectors.toList());
+        }
+
+        if (entity != null) {
+            filteredTrainings = filteredTrainings.stream()
+                    .filter(training -> training.getEntity().equals(entity))
+                    .collect(Collectors.toList());
+        }
+
+        if (topic != null) {
+            filteredTrainings = filteredTrainings.stream()
+                    .filter(training -> training.getTopic().equals(topic))
+                    .collect(Collectors.toList());
+        }
+
+        if (filteredTrainings.isEmpty()) {
+            return new ResponseEntity<>("No trainings found.", HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(filteredTrainings, HttpStatus.OK);
+    }
+
+    // }
 
     @PostMapping("/training")
     public ResponseEntity<training> createTraining(@RequestBody training training) {
@@ -72,54 +95,62 @@ public class TrainingController {
         try {
             trainingRepository.deleteById(id);
             // System.out.print("Training deleted");
-            return new ResponseEntity("Training deleted",HttpStatus.NO_CONTENT);
+            return new ResponseEntity("Training deleted", HttpStatus.NO_CONTENT);
         } catch (Exception e) {
-            return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);       
-        }
-    }
-    
-    @GetMapping("/training/name")
-    public ResponseEntity<List<training>> getTrainingByName(@RequestParam("name") String name) {
-        try {
-            List<training> trainings = trainingRepository.findByName(name);
-
-            if (trainings.isEmpty()) {
-                return new ResponseEntity("No training under this name are found. \nPlease try again.", HttpStatus.NOT_FOUND);
-            }
-            return new ResponseEntity<>(trainings, HttpStatus.OK);
-        } catch (Exception e) {
-            //return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-            throw e;
+            return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @GetMapping("/training/topic")
-    public ResponseEntity<List<training>> getTrainingByTopic(@RequestParam("topic") String topic) {
-        try {
-            List<training> trainings = trainingRepository.findByTopic(topic);
+    // @GetMapping("/training/name")
+    // public ResponseEntity<List<training>> getTrainingByName(@RequestParam("name")
+    // String name) {
+    // try {
+    // List<training> trainings = trainingRepository.findByName(name);
 
-            if (trainings.isEmpty()) {
-                return new ResponseEntity("No training under this topic are found.",HttpStatus.NOT_FOUND);
-            }
-            return new ResponseEntity<>(trainings, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+    // if (trainings.isEmpty()) {
+    // return new ResponseEntity("No training under this name are found. \nPlease
+    // try again.",
+    // HttpStatus.NOT_FOUND);
+    // }
+    // return new ResponseEntity<>(trainings, HttpStatus.OK);
+    // } catch (Exception e) {
+    // // return new ResponseEntity(e.getMessage(),
+    // HttpStatus.INTERNAL_SERVER_ERROR);
+    // throw e;
+    // }
+    // }
 
-    @GetMapping("/training/entity")
-    public ResponseEntity<List<training>> getTrainingByEntity(@RequestParam("entity") String entity) {
-        try {
-            List<training> trainings = trainingRepository.findByEntity(entity);
+    // @GetMapping("/training/topic")
+    // public ResponseEntity<List<training>>
+    // getTrainingByTopic(@RequestParam("topic") String topic) {
+    // try {
+    // List<training> trainings = trainingRepository.findByTopic(topic);
 
-            if (trainings.isEmpty()) {
-                return new ResponseEntity("No training under this entity are found.",HttpStatus.NOT_FOUND);
-            }
-            return new ResponseEntity<>(trainings, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+    // if (trainings.isEmpty()) {
+    // return new ResponseEntity("No training under this topic are found.",
+    // HttpStatus.NOT_FOUND);
+    // }
+    // return new ResponseEntity<>(trainings, HttpStatus.OK);
+    // } catch (Exception e) {
+    // return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    // }
+    // }
+
+    // @GetMapping("/training/entity")
+    // public ResponseEntity<List<training>>
+    // getTrainingByEntity(@RequestParam("entity") String entity) {
+    // try {
+    // List<training> trainings = trainingRepository.findByEntity(entity);
+
+    // if (trainings.isEmpty()) {
+    // return new ResponseEntity("No training under this entity are found.",
+    // HttpStatus.NOT_FOUND);
+    // }
+    // return new ResponseEntity<>(trainings, HttpStatus.OK);
+    // } catch (Exception e) {
+    // return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    // }
+    // }
 
     @PatchMapping("/training")
     public ResponseEntity<training> updateTraining(@RequestParam("id") long id, @RequestBody training training) {
@@ -127,28 +158,28 @@ public class TrainingController {
 
         if (trainingData.isPresent()) {
             training _training = trainingData.get();
-            if(training.name != null){
+            if (training.name != null) {
                 _training.name = training.name;
             }
-            if(training.link != null){
+            if (training.link != null) {
                 _training.link = training.link;
             }
-            if(training.duration != 0){
+            if (training.duration != 0) {
                 _training.duration = training.duration;
             }
-            if(training.topic != null){
+            if (training.topic != null) {
                 _training.topic = training.topic;
             }
-            if(training.entity != null){
+            if (training.entity != null) {
                 _training.entity = training.entity;
             }
-            if(training.AVG_Rating != 0){
+            if (training.AVG_Rating != 0) {
                 _training.AVG_Rating = training.AVG_Rating;
             }
             return new ResponseEntity<>(trainingRepository.save(_training), HttpStatus.OK);
         } else {
-            return new ResponseEntity("There are no trainings with this ID",HttpStatus.NOT_FOUND);
+            return new ResponseEntity("There are no trainings with this ID", HttpStatus.NOT_FOUND);
         }
     }
-    
+
 }
